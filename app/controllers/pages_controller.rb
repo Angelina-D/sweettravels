d378d8069a6c1a61d07dddaaf3fe898441f9c16d
Angelina Diener <angelinadiener@gmx.de>
2019-08-28 16:38:21 +0200

    added user information, changed the seeds because added enum status

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,29 @@ class PagesController < ApplicationController
   def profile
    @offers = current_user.offers
    @requests = current_user.requests
+   @user = current_user
+   @sum_requests = sum_requests
+   @sum_confirmed_offers = sum_confirmed_offers
+   @sum_donation = sum_charity
+  end
+
+  private
+
+  def sum_requests
+    current_user.requests.count
+  end
+
+  def sum_confirmed_offers
+    sum_offers = current_user.offers.where(status: :confirmed)
+    sum_offers.count
+  end
+
+  def sum_charity
+    sum_confirmed = current_user.offers.where(status: :confirmed)
+    sum_donation = 0
+    sum_confirmed.each do |offer|
+      offer.request.donation_cents += sum_donation
+    end
+    sum_donation
   end
 end
