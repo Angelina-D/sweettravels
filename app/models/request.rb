27add2340445779d753816e68c0ca5747101b109
Yann Parent <yann.parent4@gmail.com>
2019-08-28 16:16:06 +0200

    Added a lovely search bar for request by country and candy name

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,14 @@ class Request < ApplicationRecord
   belongs_to :sweet
   belongs_to :user
   has_many :offers
+
+  include PgSearch::Model
+  pg_search_scope :search_sweet_name_and_country,
+                  against: [],
+                  associated_against: {
+                    sweet: [:name, :country ]
+                  },
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/search", to: 'requests#index'
 
   resources :requests, only: [:index, :show, :new, :create, :destroy] do
     resources :offers, only: [:new, :create, :show, :edit, :update]
