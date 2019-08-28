96bcf7ba735cc2cbc8131aa41bc9b04a9acd5040
Malina Steinberg malina.steinberg@gmail.com <malina.steinberg@gmail.com>
2019-08-28 11:44:07 +0200

    Worked on the landing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :requests, :offers
+  has_many :requests
+  has_many :offers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
