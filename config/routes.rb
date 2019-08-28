231075706a41fb340187436881467ad438b49c9e
Malina Steinberg malina.steinberg@gmail.com <malina.steinberg@gmail.com>
2019-08-28 14:37:29 +0200

    Index for all sweets

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :requests, only: [:index, :show, :new, :create, :destroy] do
     resources :offers, only: [:new, :create, :show, :edit, :update]
   end
+  resources :sweets, only: [:index, :show]
 end
