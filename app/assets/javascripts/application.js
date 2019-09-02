f01aafe77593368a1423bc61f9f05817a613682f
Yann Parent <yann.parent4@gmail.com>
2019-09-02 11:25:26 +0200

    working on js for new request

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require rails-ujs
 //= require_tree .
+
+document.querySelectorAll(".form-charity-label").forEach((label) => {
+  console.log(label);
+    console.log("check");
+  label.addEventListener("click", (event) => {
+    if (label.classList.contains(".active")){
+      label.classList.remove("active");
+      console.log("blabla")
+    };
+    if (true){
+      console.log("true")
+    }
+    event.currentTarget.classList.toggle("active")
+  });
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-
+import 'jquery'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
