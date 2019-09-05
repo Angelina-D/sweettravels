904ad084fccef9a769193d537cc98c6338121d3b
Yann Parent <yann.parent4@gmail.com>
2019-09-05 15:54:07 +0200

    New side and change font size for charity on request index card

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,12 +74,26 @@ sweet8 = Sweet.create(
   category:'Choco',
   country: 'Germany',
   )
+sweet9 = Sweet.create(
+  name: 'Timtam',
+  description: 'In Australia there is not only deadly animal but chocolat biscuit to die for too!',
+  picture: 'http://www.arnotts.com/wp-content/uploads/2015/01/Tim-Tam-Chewy-Caramel.png',
+  category:'Choco',
+  country: 'Australia',
+  )
+sweet10 = Sweet.create(
+  name: 'Carambar',
+  description: 'The classic teeth breaker coming from France',
+  picture: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Carambar_Caramel.jpg',
+  category:'Teeth breaker',
+  country: 'France',
+  )
 puts "Sweets created!"
 
 ############################## USER ############################################
 user1 = User.create(
-  full_name: 'Annegret' ,
-  photo: 'https://images.halloweencostumes.com/products/51294/1-1/childs-german-girl-costume-.jpg',
+  full_name: 'Angie' ,
+  photo: 'https://avatars3.githubusercontent.com/u/52400250?s=460&v=4',
   email:'potatoe@hotmail.com',
   password: '123456',
   city: 'Berlin',
@@ -110,10 +124,10 @@ user4 = User.create(
 
 user5 = User.create(
   full_name: 'Yann' ,
-  photo: 'https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/153546296-items-dangerous-pet-birds.jpg',
+  photo: 'https://avatars.githubusercontent.com/u/51452830?s=460&v=4',
   email:'jsisevil@hotmail.com',
   password: '123456',
-  city: 'Nantes',
+  city: 'Berlin',
   )
 
 user6 = User.create(
@@ -133,11 +147,25 @@ user7 = User.create!(
   )
 
 user8 = User.create(
-  full_name: 'Dodo' ,
-  photo: 'https://media.vanityfair.com/photos/5c2fdb09ef10e32ca1332862/16:9/w_1280,c_limit/trumpshutdownraises.jpg',
-  email:'ilovecarlos@hotmail.com',
+  full_name: 'Grant' ,
+  photo: 'https://avatars2.githubusercontent.com/u/48766845?s=460&v=4',
+  email:'timtam@hotmail.com',
   password: '123456',
-  city: 'Los Angeles',
+  city: 'Melbourne',
+  )
+user9 = User.create(
+  full_name: 'Amelle' ,
+  photo: 'https://avatars2.githubusercontent.com/u/49689058?s=460&v=4',
+  email:'memelle@hotmail.com',
+  password: '123456',
+  city: 'Berlin',
+  )
+user10 = User.create(
+  full_name: 'Victor' ,
+  photo: 'https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/67557545_929840497354966_844013081422987264_o.jpg?_nc_cat=100&_nc_oc=AQm63oEcvg4oc2hmqGTzUAGWBl3X2JDhtG2kfLL6UpFnpGwS7FA_2s-Ny-kkXR5bFQ8&_nc_ht=scontent-amt2-1.xx&oh=8b0fc5be17c4adac120de056b2c48b46&oe=5E058913',
+  email:'redcabage@hotmail.com',
+  password: '123456',
+  city: 'Berlin',
   )
 
 puts "Users created!"
@@ -147,41 +175,41 @@ puts "Users created!"
 Charity.create(
   name: "Action Against Hunger",
   cause: "Education",
-  description: 'Children are the faces of the future, and their education is one of the most critical factors to help a community rise above poverty. Your gift of education will not only help a child but her entire village. ',
+  description: "Children are the faces of the future, and their education is one of the most critical factors to help a community rise above poverty. Your gift of education will not only help a child but her entire village.",
   picture: 'https://www.channelstv.com/wp-content/uploads/2019/07/ACF-3.jpg'
   )
 
 Charity.create(
-  name: "Doctor wihtout Borders",
+  name: "Doctor without Borders",
   cause: "Hunger",
-  description: 'Action Against Hunger (or Action Contre La Faim (ACF) in French) is a global humanitarian organization which originated in France and is committed to ending world hunger. The organization helps malnourished children and provides communities with access to safe water and sustainable solutions to hunger. ',
+  description: "Action Against Hunger (or Action Contre La Faim (ACF) in French) is a global humanitarian organization which originated in France and is committed to ending world hunger. The organization helps malnourished children and provides communities with access to safe water and sustainable solutions to hunger.",
   picture: 'https://armacad.info/images/2016/07/doctors-without-borders-k8t3CTUSaD1UHYV2pTmtzyK1Q78nN9zW.png'
   )
 Charity.create(
-  name: "Action Against Hunger",
+  name: "World diabetes foundation",
+  cause: "diabetes",
+  description: "Fighting diabetes worldwide. Our vision is to alleviate suffering related to diabetes and its complications among those least able to withstand the burden",
+  picture: 'http://www.hereweareglobal.com/wp-content/uploads/2018/06/WDF-logo-standard.jpg'
+  )
+
+Charity.create(
+  name: "Rise Against Hunger",
+  cause: "Hunger",
+  description: "Rise Against Hunger is an international hunger relief non-profit organization that coordinates the packaging and distribution of food and other life-changing aid to people in developing nations",
+  picture: 'https://weag.church/wp-content/uploads/2019/03/rise-against-hunger.png'
+  )
+Charity.create(
+  name: "UNICEF",
   cause: "Education",
-  description: 'Children are the faces of the future, and their education is one of the most critical factors to help a community rise above poverty. Your gift of education will not only help a child but her entire village. ',
-  picture: 'https://www.channelstv.com/wp-content/uploads/2019/07/ACF-3.jpg'
+  description: "The United Nations Children's Fund, originally known as the United Nations International Children's Emergency Fund, was created by the United Nations General Assembly on 11 December 1946, to provide emergency food and healthcare to children and mothers in countries that had been devastated by World War II",
+  picture: 'http://www.developmentchannel.org/wp-content/uploads/2018/10/UNICEF-Logo.jpg'
   )
 
 Charity.create(
-  name: "Doctor wihtout Borders",
-  cause: "Hunger",
-  description: 'Action Against Hunger (or Action Contre La Faim (ACF) in French) is a global humanitarian organization which originated in France and is committed to ending world hunger. The organization helps malnourished children and provides communities with access to safe water and sustainable solutions to hunger. ',
-  picture: 'https://armacad.info/images/2016/07/doctors-without-borders-k8t3CTUSaD1UHYV2pTmtzyK1Q78nN9zW.png'
-  )
-Charity.create(
-  name: "Action Against Hunger",
-  cause: "Education",
-  description: 'Children are the faces of the future, and their education is one of the most critical factors to help a community rise above poverty. Your gift of education will not only help a child but her entire village. ',
-  picture: 'https://www.channelstv.com/wp-content/uploads/2019/07/ACF-3.jpg'
-  )
-
-Charity.create(
-  name: "Doctor wihtout Borders",
-  cause: "Hunger",
-  description: 'Action Against Hunger (or Action Contre La Faim (ACF) in French) is a global humanitarian organization which originated in France and is committed to ending world hunger. The organization helps malnourished children and provides communities with access to safe water and sustainable solutions to hunger. ',
-  picture: 'https://armacad.info/images/2016/07/doctors-without-borders-k8t3CTUSaD1UHYV2pTmtzyK1Q78nN9zW.png'
+  name: "Green Peace",
+  cause: "Environment",
+  description: "Greenpeace is a non-governmental environmental organization with offices in over 39 countries and an international coordinating body in Amsterdam, the Netherlands. Greenpeace was founded in 1971 by Irving Stowe and Dorothy Stowe, Canadian and US ex-pat environmental activists",
+  picture: 'https://www.greenpeace.org/international/wp-content/themes/planet4-master-theme/images/Greenpeace-logo.png'
   )
 
 puts "Charity created!"
@@ -243,7 +271,7 @@ Request.create(
     quantity: rand(1..5),
     charity_id: Charity.all.sample.id,
     user: user8,
-    sweet: sweet7,
+    sweet: sweet9,
     )
 
 Request.create(
@@ -262,8 +290,17 @@ Request.create!(
     donation_cents: rand(500..2000),
     quantity: rand(1..5),
     charity_id: Charity.all.sample.id,
-    user: user1,
+    user: user9,
     sweet: sweet8,
+    )
+Request.create!(
+    description: 'My all time Favorite - send help pls',
+    price_cents: rand(1000..2500),
+    donation_cents: rand(500..2000),
+    quantity: rand(1..5),
+    charity_id: Charity.all.sample.id,
+    user: user10,
+    sweet: sweet10,
     )
 puts "Requests created!"
 
