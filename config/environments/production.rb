259fc1a6a575dc7d47f6667a3c5a893c4fa7a6c1
Angelina Diener <angelinadiener@gmx.de>
2019-09-05 12:23:11 +0200

    Fixing cable thigns

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://sweet-travels.world/cable'
-  config.action_cable.allowed_request_origins = ['http://www.sweet-travels.world', 'https://www.sweet-travels.world']
+  config.action_cable.url = 'ws://www.sweet-travels.world/cable'
+  config.action_cable.allowed_request_origins = ['http://www.sweet-travels.world', 'sweet-travels.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
